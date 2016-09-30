b8588caabb98b318070c71a6ea18f61cce05924d
dryabtse <tain.furrious@gmail.com>
2016-09-30 10:17:06 +1000

    "First draft"

---
+++ mmsClient.py
@@ -0,0 +1,103 @@
+from requests.auth import HTTPDigestAuth
+import requests
+import json
+ 
+ 
+class MmsClient:
+    def __init__(self, username, apiKey, baseUrl):
+        self.username = username
+        self.apiKey = apiKey
+        self.url = baseUrl + '/api/public/v1.0/'
+ 
+    def getAutomationConfig(self, groupId):
+        url = self.url + 'groups/' + groupId + '/automationConfig'
+        result = requests.get(url, auth=HTTPDigestAuth(self.username, self.apiKey))
+        return json.loads(result.text)
+        
+    def getHosts(self, groupId):
+        url = self.url + 'groups/' + groupId + '/hosts'
+        result = requests.get(url, auth=HTTPDigestAuth(self.username, self.apiKey))
+        return json.loads(result.text)
+    
+    def getHostByNameAndPort(self, groupId, hostname, port):
+        url = self.url + 'groups/' + groupId + '/hosts/byName/' + hostname + ':' + port
+        result = requests.get(url, auth=HTTPDigestAuth(self.username, self.apiKey))
+        return json.loads(result.text)
+    
+    def delHost(self, groupId, hostId):
+        url = self.url + 'groups/' + groupId + '/hosts/' + hostId
+        result = requests.delete(url, auth=HTTPDigestAuth(self.username, self.apiKey))
+        return json.loads(result.text)
+ 
+    def updateAutomationConfig(self, groupId, automationConfig):
+        url = self.url + 'groups/' + groupId + '/automationConfig'
+        headers = {'Content-type': 'application/json'}
+        result = requests.put(url, auth=HTTPDigestAuth(self.username, self.apiKey), json=automationConfig, headers=headers)
+        return json.loads(result.text)
+        
+    def putMonAgentInfo(self, groupId, agentConfig):
+        url = self.url + 'groups/' + groupId + '/automationConfig/monitoringAgentConfig' 
+        headers = {'Content-type': 'application/json'}
+        result = requests.put(url, auth=HTTPDigestAuth(self.username, self.apiKey), json=agentConfig, headers=headers)
+        return json.loads(result.text)
+        
+    def putBackAgentInfo(self, groupId, agentConfig):
+        url = self.url + 'groups/' + groupId + '/automationConfig/backupAgentConfig' 
+        headers = {'Content-type': 'application/json'}
+        result = requests.put(url, auth=HTTPDigestAuth(self.username, self.apiKey), json=agentConfig, headers=headers)
+        return json.loads(result.text)
+    
+    def getAgents(self, groupId):
+        url = self.url + 'groups/' + groupId + '/agents'
+        result = requests.get(url, auth=HTTPDigestAuth(self.username, self.apiKey))
+        return json.loads(result.text)
+    
+    def agentByType(self, groupId, type):
+        # Todo - check the type value
+        url = self.url + 'groups/' + groupId + '/agents/' + type
+        result = requests.get(url, auth=HTTPDigestAuth(self.username, self.apiKey))
+        return json.loads(result.text)
+ 
+
+username = "dmitry.ryabtsev@10gen.com"
+apiKey = "eff7c093-fcae-4355-b1f5-3164490e962a"
+baseUrl = "http://52.64.222.163:8080"
+mmsClient = MmsClient(username, apiKey, baseUrl)
+
+groupId = "57ad6050e4b0551a0483b82b"
+
+
+##### Main section
+automationConfig = mmsClient.getAutomationConfig(groupId)
+print(json.dumps(automationConfig["version"]))
+
+#To remove a node from replica set:
+#
+#del automationConfig["processes"][2]["args2_6"]["replication"]
+#automationConfig["replicaSets"][0]["members"].pop(2)
+#automationConfig["version"] = int(automationConfig["version"]) + 1
+#updateResult = mmsClient.updateAutomationConfig(groupId, automationConfig)
+#print(json.dumps(updateResult))
+
+#f( len(automationConfig["processes"]) > 0):
+   # automationConfig["processes"][0]["disabled"] = True
+    #automationConfig["version"] = int(automationConfig["version"]) + 1
+    #updateResult = mmsClient.updateAutomationConfig(groupId, automationConfig)
+    #print(json.dumps(updateResult))
+
+hosts = mmsClient.getHosts(groupId)
+for host in hosts["results"]:
+    print(json.dumps(host["id"]))
+    print(json.dumps(host["hostname"]))
+    print(json.dumps(host["port"]))
+
+mmsClient.delHost(groupId, id)
+
+hostname = "ip-172-31-9-206.ap-southeast-2.compute.internal"
+port = "27015"
+
+host = mmsClient.getHostByNameAndPort(groupId, hostname, port)
+
+
+# To stop a process 
+# automationConfig["processes"][5]["disabled"] = True
